26dadec3d5f54c6c7f28fb4d08614fa440d89799
mbonheur7 <m.bonheur1@alustudent.com>
2026-07-27 09:30:09 +0200

    neu

--- student_menu.py
+++ student_menu.py
@@ -1,36 +1,38 @@
-from student import add_student_menu, view_students
+from student import add_student_menu, view_students, search_student_menu, update_stdent_menu, delete_student_menu
 from subjects_module import subject_modules
 from grade import grades_module
 from report import report_module
 
 def student_menu():
     while True:
-        print("\n===== STUDENT MENU =====")
-        print("1. Add Student")
-        print("2. View Students")
-        print("0. Back")
+      print("\n===== Student Menu =====")
+      print("1. Add Student")
+      print("2. View Students")
+      print("3. Search Student")
+      print("4. Update Student")
+      print("5. Delete Student")
+      print("0. Back")
 
-        choice = input("Enter choice: ")
+      choice = input("Enter your choice: ")
 
-        if choice == "1":
-            add_student_menu()
+      if choice == "1":
+        add_student_menu()
 
-        elif choice == "2":
-            view_students()
+      elif choice == "2":
+        view_students()
 
-        elif choice == "0":
-            break
+      elif choice == "3":
+        search_student_menu()
 
-        else:
-            print("Invalid choice.")
+      elif choice == "4":
+        update_student_menu()
 
-def subject_menu():
-    subject_modules()
+      elif choice == "5":
+        delete_student_menu()
 
+      elif choice == "0":
+        print("Goodbye!")
+        break
 
-def grade_menu():
-    grades_module()
-
-
-def report_menu():
-    report_module()
+    else:
+        print("Invalid choice. Please try again.")
